077186b49754f57bb35282fb44bcc6cef7f751f9
janayasiin <janayasiin@gmail.com>
2026-07-23 20:03:27 +0300

    Initial commit

---
+++ Week 01/Day 01/HelloBinx/HelloBinx/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloBinx
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jana Yassin" + ", " + DateTime.Now);
+        }
+    }
+}
